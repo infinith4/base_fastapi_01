21b05a6cd035335e2d816352ae29a3c6d9033e2b
infinith4 <infinith4@gmail.com>
2023-07-17 16:51:42 +0900

    add test

--- fast_app/tests/test_main.py
+++ fast_app/tests/test_main.py
@@ -28,3 +28,11 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"msg": "Hello World"}
+
+def test_read_item():
+    response = client.get("/items/1?q=test")
+    assert response.status_code == 200
+    assert response.json() == {
+                    "item_id": 1,
+                    "q": "test"
+                }
